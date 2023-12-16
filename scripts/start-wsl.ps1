86f8ff92bf2f64f15f26212c163212dbd9dc5074
ninbura <gabriel@ninbura.com>
2023-12-16 01:03:41 -0700

    added logging to start-wsl script

--- scripts/start-wsl.ps1
+++ scripts/start-wsl.ps1
@@ -1,6 +1,22 @@
+function writeToLog(){
+  $logPath = "$PSScriptRoot/../start-wsl.log"
+
+  if(!(test-path $logPath)){
+    new-item -Path $logPath -ItemType File -Force -Verbose
+  }
+
+  $date = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+  $logEntry = "[$date] - Script has run successfully.`n"
+
+  Add-Content -Path $logPath -Value $logEntry -Verbose
+}
+
 function main() {
   $config = Get-Content -Path "$PSScriptRoot/../config.json" -Raw | ConvertFrom-Json
+  
   wsl -d $config.imageName
+
+  writeToLog
 }
 
 main
