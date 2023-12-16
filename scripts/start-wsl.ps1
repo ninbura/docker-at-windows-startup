21607e640a6d886fcaa27f948546dfe010b36409
ninbura <gabriel@ninbura.com>
2023-12-16 01:24:38 -0700

    Fixed logging.

--- scripts/start-wsl.ps1
+++ scripts/start-wsl.ps1
@@ -1,22 +1,43 @@
-function writeToLog(){
-  $logPath = "$PSScriptRoot/../start-wsl.log"
+param(
+  [string]$testRelativePath = "c:\repos\docker-at-windows-startup"
+)
+
+function getRelativePath(){
+  try{
+    $relativePath = $(Split-Path $PSScriptRoot -Parent)
+
+    if($null -eq $relativePath){
+      throw "Could not get relative path."
+    }
+  } catch {
+    $relativePath = $testRelativePath
+  }
+
+  write-host "$relativePath" -ForegroundColor Magenta
+
+  return $relativePath
+}
+
+function writeToLog($relativePath, $scriptName){
+  $logPath = "$relativePath/$scriptName.log"
 
   if(!(test-path $logPath)){
     new-item -Path $logPath -ItemType File -Force -Verbose
   }
 
   $date = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
-  $logEntry = "[$date] - Script has run successfully.`n"
+  $logEntry = "[$date] - $scriptName script ran successfully.`n"
 
   Add-Content -Path $logPath -Value $logEntry -Verbose
 }
 
 function main() {
-  $config = Get-Content -Path "$PSScriptRoot/../config.json" -Raw | ConvertFrom-Json
+  $relativePath = getRelativePath
+  $config = Get-Content -Path "$relativePath/config.json" -Raw | ConvertFrom-Json
+  $scriptName = "start-wsl"
+  writeToLog $relativePath $scriptName
   
   wsl -d $config.imageName
-
-  writeToLog
 }
 
 main
